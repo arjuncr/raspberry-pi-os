adeca48fa325d75539d433eeeaa3f993266754eb
arjuncr <arjuncr00@gmail.com>
2019-04-05 00:17:52 +0530

    initial commit

--- download.sh
+++ download.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-rm -r sources
+SOURCE="sources"
+
+if [ -d ${SOURCE} ];
+then
+rm -r $SOURCE
+fi
+
+mkdir $SOURCE
 
 git clone https://github.com/arjuncr/light-os.git
+ 
+git clone --depth=1 https://github.com/arjuncr/linux ./$SOURCE/linux
 
-mkdir sources
+git clone --depth=1 https://github.com/arjuncr/raspberry-pi-uboot.git ./$SOURCE/uboot
 
-git clone --depth=1 https://github.com/arjuncr/linux ./sources/linux
-
-git clone --depth=1 https://github.com/arjuncr/raspberry-pi-uboot.git ./sources/uboot
-
-mv  light-os/* sources/
+mv  light-os/* $SOURCE/
 
 rm -r light-os
--- install_qemu_arm.sh
+++ install_qemu_arm.sh
@@ -2,11 +2,35 @@
 
 QEMU="3.1.0"
 
-wget https://download.qemu.org/qemu-$QEMU.tar.xz
+if [ "$1" == "-c" ]
+then
+        if [ -f qemu-$QEMU.tar.xz ]
+        then
+                rm qemu-$QEMU.tar.xz
+        fi
+        if [ -d qemu-$QEMU ]
+        then
+                rm -r qemu-$QEMU
+        fi
+fi
 
-tar xvJf qemu-$QEMU.tar.xz
+if [ ! -f qemu-$QEMU.tar.xz ]
+then
+	wget https://download.qemu.org/qemu-$QEMU.tar.xz
+	if [ ! -d qemu-$QEMU ]
+	then	
+		tar xvJf qemu-$QEMU.tar.xz
+	fi
+else
+	if [ ! -d qemu-$QEMU ]
+        then
+        	tar xvJf qemu-$QEMU.tar.xz
+        fi
+fi
 
 cd qemu-$QEMU
-./configure --target-list=arm-softmmu,arm-linux-user
-make -j 2
-sudo make install
+	make clean
+	./configure --target-list=arm-softmmu,arm-linux-user
+	make -j 4 
+	make install
+
